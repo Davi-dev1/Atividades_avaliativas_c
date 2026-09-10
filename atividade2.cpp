b675e6bf8d2a720231529def5e13a1a9fb0e7a82
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-09-10 11:13:01 -0300

    Create atividade2.cpp

---
+++ atividade2.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ 
+  
+    int a=5;
+    int b=10;
+    int temp;
+    
+    
+  printf("Antes da Troca : a = %i, b= %i",a,b );
+  
+  temp = a;
+  
+  a = b;
+  b = temp ; 
+  
+  printf("Depois da troca a = %i,b=%i",a,b);
+  
+ 
+  
+  
+    
+   
+    
+    
+    
+    
+    
+
+
+}
