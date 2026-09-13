eb0434d761b1307cf0db462097577f2dae7153da
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-09-13 01:46:33 -0300

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    //TODO : Calcule e exiba a soma de todos os números pares no intervalo de 1 a 100 utilizando a estrutura for
+
+   
+    for(int i=0;i<=100;i=i+2){
+        
+        
+        
+        printf("\n%i",i);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
